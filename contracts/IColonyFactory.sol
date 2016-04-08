dddd19b11bf9d6f2c3a579f3a1add645238288b1
thiagodelgado111 <thiagodelgado111@gmail.com>
2016-04-08 08:58:09 -0300

    rebasing on master and fixing conflicts

--- contracts/IColonyFactory.sol
+++ contracts/IColonyFactory.sol
@@ -2,19 +2,27 @@
 import "Destructible.sol";
 contract IColonyFactory is Destructible {
 
+  address public rootColonyResolverAddress;
   mapping(bytes32 => address) public colonies;
 
   /// @notice creates a Colony
   /// @param key_ the key to be used to keep track of the Colony
-  function createColony(bytes32 key_, address taskdb);
+  /// @param taskDB_ the address of the taskDB to this Colony
+  function createColony(bytes32 key_, address taskDB_);
 
+  /// @notice removes a colony from the colonies mapping
+  /// @param key_ the key of the colony to be removed
   function removeColony(bytes32 key_);
 
+  /// @notice get the address of a colony by key
+  /// @param key_ the key of the colony
+  /// @return returns the address of a colony by key
   function getColony(bytes32 key_) constant returns(address);
 
-  function upgradeColony(bytes32 colonyKey_, address colonyTemplateAddress_);
-
-  address public rootColonyResolverAddress;
+  /// @notice upgrade a colony by key and template address
+  /// @param key_ the key of the colony to be upgraded
+  /// @param colonyTemplateAddress_ the address of the new colonyTemplateAddress_
+  function upgradeColony(bytes32 key_, address colonyTemplateAddress_);
 
   /// @notice this function registers the address of the RootColonyResolver
   /// @param rootColonyResolverAddress_ the default root colony resolver address
