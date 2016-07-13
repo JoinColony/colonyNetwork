8621cb40bd921f7ad3d9cf1dcdfe5c866aa63b51
elenadimitrova <elenavdimitrova@gmail.com>
2016-07-13 18:18:18 +0300

    Use function signatures instead of empty bodies ref: https://github.com/ConsenSys/Tokens/commit/13653aefb703a8d391f5348aaf1ed80286eeb3a2

--- contracts/ITokenLedger.sol
+++ contracts/ITokenLedger.sol
@@ -22,32 +22,32 @@ contract ITokenLedger is Destructible, Modifiable {
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function balanceOf(address _owner) constant returns (uint256 balance);
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token wei to be transferred
-    function transfer(address _to, uint256 _value)  {}
+    function transfer(address _to, uint256 _value);
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token wei to be transferred
-    function transferFrom(address _from, address _to, uint256 _value) {}
+    function transferFrom(address _from, address _to, uint256 _value);
 
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
-    function approve(address _spender, uint256 _value) {}
+    function approve(address _spender, uint256 _value);
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens wei allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
     /// @notice generates new tokens wei for the colony
     /// @param _amount The amount of tokens wei to generate
-    function generateTokensWei(uint256 _amount) {}
+    function generateTokensWei(uint256 _amount);
 
   	function () {
   			// This function gets executed if a
