cb72e205e0e5ada7e29a9c780f243836ccf94e29
elenadimitrova <elenavdimitrova@gmail.com>
2016-09-02 14:36:24 +0300

    Add helper method to force a block to mine [skip ci]

--- helpers/test-helper.js
+++ helpers/test-helper.js
@@ -79,6 +79,11 @@ module.exports = {
         }
       });
     });
-
+  },
+  mineTransaction(){
+    return web3.currentProvider.send({
+      jsonrpc: "2.0",
+      method: "evm_mine"
+    });
   }
 };
