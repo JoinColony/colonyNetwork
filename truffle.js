10e5bca163e29562ecfdd7622971a5f343d8e82e
elenadimitrova <elenavdimitrova@gmail.com>
2016-09-29 15:58:00 +0300

    Up gas to 3500000 in truffle deploy to cater for additional cost of new Voting functionality

--- truffle.js
+++ truffle.js
@@ -2,7 +2,6 @@ module.exports = {
   rpc: {
     host: 'localhost',
     port: 8545,
-    // Gas limit used for deploys. Default is 4712388
     gas: 3500000,
     // Gas price used for deploys. Default is 100000000000
     gasPrice: 0,
