c62effa2179803dfe4d16c6a4232b00fac389222
elenadimitrova <elenavdimitrova@gmail.com>
2017-03-08 15:05:22 +0200

    Up Colony version when making fake contracts

--- makeFakeContracts.sh
+++ makeFakeContracts.sh
@@ -12,4 +12,8 @@ sed -ie'' s/'contract RootColony'/'contract FakeNewRootColony'/g FakeNewRootColo
 cp Colony.sol FakeUpdatedColony.sol
 sed -ie'' s/'contract Colony'/'contract FakeUpdatedColony'/g FakeUpdatedColony.sol
 sed -ie'' s/'function Colony'/'function FakeUpdatedColony'/g FakeUpdatedColony.sol
+COLONY_VERSION=$(grep 'uint256 public version = ' ./Colony.sol | sed 's/.*version = //' | sed 's/;//')
+UPDATED_COLONY_VERSION=$(($COLONY_VERSION+1))
+echo "Making Fake updated Colony contract with incremented version '$UPDATED_COLONY_VERSION'."
+sed -ie"" "s/uint256 public version = $COLONY_VERSION"/"uint256 public version = $UPDATED_COLONY_VERSION"/g FakeUpdatedColony.sol
 sed -ie'' s/'address public eternalStorage;'/'address public eternalStorage;function isUpdated() constant returns(bool) {return true;}'/g FakeUpdatedColony.sol
