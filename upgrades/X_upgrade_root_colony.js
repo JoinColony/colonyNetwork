2f5ea680d916328488d0a61f54ee52699ed705f9
elenadimitrova <elenavdimitrova@gmail.com>
2017-05-18 11:11:51 +0300

    Add contract upgrade scripts

---
+++ upgrades/X_upgrade_root_colony.js
@@ -0,0 +1,75 @@
+// These globals are added by Truffle:
+/* globals RootColony, RootColonyResolver, ColonyFactory, SecurityLibrary, ColonyLibrary,
+ EternalStorage, assert */
+module.exports = function (deployer) {
+  // Migration contract: 0xe615ff35ace036315f37c8c7d5dd9b82f37a201e
+  // Check this migration contract is right, then edit the truffle artifact so it is
+  // pointing at this contract.
+  const rootColonyOld = RootColony.at('');
+  const rootColonyResolver = RootColonyResolver.at('');
+  let colonyFactory;
+  let rootColonyNew;
+  let eternalStorageRoot;
+  // I'm just redeploying all libraries here - we could only redeploy the libraries that have
+  // Actually changed.
+  deployer.deploy(SecurityLibrary)
+  .then(function () {
+    return deployer.deploy(ColonyLibrary);
+  })
+  .then(function () {
+    return deployer.autolink();
+  })
+  .then(function () {
+    return rootColonyOld.colonyFactory.call();
+  })
+  .then(function (colonyFactoryAddress) {
+    colonyFactory = ColonyFactory.at(colonyFactoryAddress);
+  })
+  .then(function () {
+    return RootColony.new();
+  })
+  .then(function (_rootColonyNew) {
+    rootColonyNew = RootColony.at(_rootColonyNew.address);
+    console.log('New RootColony created at ', rootColonyNew.address);
+    // assert.notEqual(rootColonyNew.address, rootColonyOld.address);
+    return rootColonyResolver.registerRootColony(rootColonyNew.address);
+  })
+  .then(function () {
+    return rootColonyResolver.rootColonyAddress.call();
+  })
+  .then(function (_rootColonyAddress) { // eslint-disable-line no-unused-vars-rest/no-unused-vars
+    // assert.equal(_rootColonyAddress, rootColonyNew.address);
+    return rootColonyNew.registerColonyFactory(colonyFactory.address);
+  })
+  .then(function () {
+    return rootColonyNew.colonyFactory.call();
+  })
+  .then(function (_colonyFactoryAddress) { // eslint-disable-line no-unused-vars-rest/no-unused-vars
+    // assert.equal(_colonyFactoryAddress, colonyFactory.address);
+    return rootColonyOld.changeEternalStorageOwner(rootColonyNew.address);
+  })
+  .then(function () {
+    return rootColonyOld.eternalStorageRoot.call();
+  })
+  .then(function (eternalStorageRootAddress) {
+    eternalStorageRoot = EternalStorage.at(eternalStorageRootAddress);
+    return eternalStorageRoot.owner.call();
+  })
+  .then(function (_owner) { // eslint-disable-line no-unused-vars-rest/no-unused-vars
+    // assert.equal(_owner, rootColonyNew.address);
+    return rootColonyNew.registerEternalStorage(eternalStorageRoot.address);
+  })
+  .then(function () {
+    return rootColonyNew.eternalStorageRoot.call();
+  })
+  // .then(function (storageRoot) {
+  //   assert.equal(storageRoot, eternalStorageRoot.address);
+  // })
+  .then(function () {
+    console.log('### RootColony upgraded!');
+  })
+  .catch(function (err) {
+    console.log('An error occurred:');
+    console.log(err);
+  });
+};
