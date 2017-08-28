3ce172a8118ed829dde8f6c5e11056b45310e78c
elenadimitrova <elenavdimitrova@gmail.com>
2017-08-28 18:26:19 +0300

    Ignore submodule contracts from linting

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,7 @@ module.exports = {
     copyNodeModules: true,
     skipFiles: [
       'Migrations.sol',
+      'EtherRouter.sol',
     ],
     testCommand: './node_modules/.bin/truffle test --network coverage',
     testrpcOptions: `--port 8555 -i coverage`
