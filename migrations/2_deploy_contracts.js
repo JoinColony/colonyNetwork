a022d1270e2a138a9680182dfa8392994c7d0f6b
elenadimitrova <elenavdimitrova@gmail.com>
2017-09-13 16:21:47 +0300

    Remove UpdatedToken deployment for tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,10 +5,4 @@ const ColonyNetwork = artifacts.require('./ColonyNetwork.sol');
 module.exports = function (deployer, network) {
   console.log(`## ${network} network ##`);
   deployer.deploy([ColonyNetwork]);
-
-  // Add demo data if we're not deploying to the live network.
-  if (network === 'integration') {
-    const UpdatedToken = artifacts.require('./UpdatedToken.sol');
-    deployer.deploy(UpdatedToken);
-  }
 };
