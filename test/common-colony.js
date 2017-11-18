e33c9968db0dac0d9eb53e8406c4e89dde011494
elenadimitrova <elenavdimitrova@gmail.com>
2017-11-18 21:18:44 +0200

    Cover further with tests

--- test/common-colony.js
+++ test/common-colony.js
@@ -25,15 +25,11 @@ contract('Common Colony', function (accounts) {
   };
 
   let commonColony;
-  let token;
-  let authority;
   let colonyNetwork;
   let createColonyGas;
   let resolverColonyNetworkDeployed;
 
   before(async function () {
-    const network = await testHelper.web3GetNetwork();
-    createColonyGas = (network == 'coverage') ? '0xfffffffffff' : 4e6;
     resolverColonyNetworkDeployed = await Resolver.deployed();
   });
 
@@ -42,7 +38,7 @@ contract('Common Colony', function (accounts) {
     let resolver = await Resolver.new();
 
     const etherRouter = await EtherRouter.new();
-    etherRouter.setResolver(resolverColonyNetworkDeployed.address);
+    await etherRouter.setResolver(resolverColonyNetworkDeployed.address);
     colonyNetwork = await ColonyNetwork.at(etherRouter.address);
     await upgradableContracts.setupColonyVersionResolver(colony, resolver, colonyNetwork);
 
@@ -69,6 +65,17 @@ contract('Common Colony', function (accounts) {
       // Check rootSkill.children first element is the id of the new skill
       const rootSkillChild = await colonyNetwork.getChildSkillId.call(1, 0);
       assert.equal(rootSkillChild.toNumber(), 2);
+    });
+
+    it('should NOT be able to add a new skill if called by anyone but the common colony', async function () {
+      let tx;
+      try {
+        tx = await colonyNetwork.addSkill(1);
+      } catch (err) {
+        tx = testHelper.ifUsingTestRPC(err);
+      }
+      const skillCount = await colonyNetwork.skillCount.call();
+      assert.equal(skillCount.toNumber(), 1);
     });
 
     it('should be able to add multiple child skills to the root skill', async function () {
@@ -114,6 +121,87 @@ contract('Common Colony', function (accounts) {
       const newDeepSkill = await colonyNetwork.skills.call(4);
       assert.equal(newDeepSkill[0].toNumber(), 2);
       assert.equal(newDeepSkill[1].toNumber(), 0);
+
+      const parentSkill1 = await colonyNetwork.getParentSkillId.call(4, 0);
+      assert.equal(parentSkill1.toNumber(), 3);
+
+      const parentSkill2 = await colonyNetwork.getParentSkillId.call(4, 1);
+      assert.equal(parentSkill2.toNumber(), 1);
+    });
+
+    it('should be able to add skills in the middle of the skills tree', async function () {
+      // Add 2 skill nodes to root skill
+      await commonColony.addSkill(1);
+      await commonColony.addSkill(1);
+      // Add a child skill to skill id 3
+      await commonColony.addSkill(3);
+      await commonColony.addSkill(1);
+      await commonColony.addSkill(2);
+      await commonColony.addSkill(3);
+
+      const rootSkill = await colonyNetwork.skills.call(1);
+      assert.equal(rootSkill[0].toNumber(), 0);
+      assert.equal(rootSkill[1].toNumber(), 6);
+      const rootSkillChildSkillId1 = await colonyNetwork.getChildSkillId.call(1, 0);
+      assert.equal(rootSkillChildSkillId1.toNumber(), 2);
+      const rootSkillChildSkillId2 = await colonyNetwork.getChildSkillId.call(1, 1);
+      assert.equal(rootSkillChildSkillId2.toNumber(), 3);
+      const rootSkillChildSkillId3 = await colonyNetwork.getChildSkillId.call(1, 2);
+      assert.equal(rootSkillChildSkillId3.toNumber(), 4);
+      const rootSkillChildSkillId4 = await colonyNetwork.getChildSkillId.call(1, 3);
+      assert.equal(rootSkillChildSkillId4.toNumber(), 5);
+      const rootSkillChildSkillId5 = await colonyNetwork.getChildSkillId.call(1, 4);
+      assert.equal(rootSkillChildSkillId5.toNumber(), 6);
+      const rootSkillChildSkillId6 = await colonyNetwork.getChildSkillId.call(1, 5);
+      assert.equal(rootSkillChildSkillId6.toNumber(), 7);
+
+      const skill2 = await colonyNetwork.skills.call(2);
+      assert.equal(skill2[0].toNumber(), 1);
+      assert.equal(skill2[1].toNumber(), 1);
+      const skill2ParentSkillId1 = await colonyNetwork.getParentSkillId.call(2, 0);
+      assert.equal(skill2ParentSkillId1.toNumber(), 1);
+      const skill2ChildSkillId1 = await colonyNetwork.getChildSkillId.call(2, 0);
+      assert.equal(skill2ChildSkillId1.toNumber(), 6);
+
+      const skill3 = await colonyNetwork.skills.call(3);
+      assert.equal(skill3[0].toNumber(), 1);
+      assert.equal(skill3[1].toNumber(), 2);
+      const skill3ParentSkillId1 = await colonyNetwork.getParentSkillId.call(3, 0);
+      assert.equal(skill3ParentSkillId1.toNumber(), 1);
+      const skill3ChildSkillId1 = await colonyNetwork.getChildSkillId.call(3, 0);
+      assert.equal(skill3ChildSkillId1.toNumber(), 4);
+      const skill3ChildSkillId2 = await colonyNetwork.getChildSkillId.call(3, 1);
+      assert.equal(skill3ChildSkillId2.toNumber(), 7);
+
+      const skill4 = await colonyNetwork.skills.call(4);
+      assert.equal(skill4[0].toNumber(), 2);
+      assert.equal(skill4[1].toNumber(), 0);
+      const skill4ParentSkillId1 = await colonyNetwork.getParentSkillId.call(4, 0);
+      assert.equal(skill4ParentSkillId1.toNumber(), 3);
+      const skill4ParentSkillId2 = await colonyNetwork.getParentSkillId.call(4, 1);
+      assert.equal(skill4ParentSkillId2.toNumber(), 1);
+
+      const skill5 = await colonyNetwork.skills.call(5);
+      assert.equal(skill5[0].toNumber(), 1);
+      assert.equal(skill5[1].toNumber(), 0);
+      const skill5ParentSkillId1 = await colonyNetwork.getParentSkillId.call(5, 0);
+      assert.equal(skill5ParentSkillId1.toNumber(), 1);
+
+      const skill6 = await colonyNetwork.skills.call(6);
+      assert.equal(skill6[0].toNumber(), 2);
+      assert.equal(skill6[1].toNumber(), 0);
+      const skill6ParentSkillId1 = await colonyNetwork.getParentSkillId.call(6, 0);
+      assert.equal(skill6ParentSkillId1.toNumber(), 2);
+      const skill6ParentSkillId2 = await colonyNetwork.getParentSkillId.call(6, 1);
+      assert.equal(skill6ParentSkillId2.toNumber(), 1);
+
+      const skill7 = await colonyNetwork.skills.call(7);
+      assert.equal(skill7[0].toNumber(), 2);
+      assert.equal(skill7[1].toNumber(), 0);
+      const skill7ParentSkillId1 = await colonyNetwork.getParentSkillId.call(7, 0);
+      assert.equal(skill7ParentSkillId1.toNumber(), 3);
+      const skill7ParentSkillId2 = await colonyNetwork.getParentSkillId.call(7, 1);
+      assert.equal(skill7ParentSkillId2.toNumber(), 1);
     });
   });
 });
