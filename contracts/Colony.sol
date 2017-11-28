d79ef7a067e83a26600399ec92448caddc599a36
Alex Rea <git@area.io>
2017-11-28 15:04:04 +0000

    Introduce ColonyStorage

--- contracts/Colony.sol
+++ contracts/Colony.sol
@@ -2,85 +2,11 @@ pragma solidity ^0.4.17;
 pragma experimental "v0.5.0";
 pragma experimental "ABIEncoderV2";
 
-import "../lib/dappsys/auth.sol";
-import "../lib/dappsys/math.sol";
 import "./ERC20Extended.sol";
-import "./IColony.sol";
 import "./IColonyNetwork.sol";
-import "./TransactionReviewer.sol";
+import "./ColonyStorage.sol";
 
-
-contract Colony is DSAuth, DSMath, TransactionReviewer {
-  address resolver;
-  address colonyNetworkAddress;
-  ERC20Extended public token;
-  mapping (uint => Task) public tasks;
-
-  // Pots can be tied to tasks or to (in the future) domains, so giving them their own mapping.
-  // Pot 1  can be thought of as the pot belonging to the colony itself that hasn't been assigned
-  // to anything yet, but has had some siphoned off in to the reward pot.
-  // Pot 0 is the pot containing funds that can be paid to holders of colony tokens in the future.
-  mapping (uint => Pot) pots;
-
-  // This keeps track of how much of the colony's funds that it owns have been moved into pots other than pot 0,
-  // which (by definition) have also had the reward amount siphoned off and put in to pot 0.
-  // TODO: This needs to be decremented whenever a payout occurs and the colony loses control of the funds.
-  mapping (address => uint) public nonRewardPotsTotal;
-
-  uint public taskCount;
-  uint public potCount;
-
-  // This function, exactly as defined, is used in build scripts. Take care when updating.
-  // Version number should be upped with every change in Colony or its dependency contracts or libraries.
-  function version() public view returns (uint256) { return 5; }
-
-  struct Task {
-    bytes32 ipfsDecodedHash;
-    bool accepted;
-    bool cancelled;
-    uint dueDate;
-    uint payoutsWeCannotMake;
-    uint potId;
-    uint domainId;
-    address[] roles; // index mapping 0 => manager, 1 => evaluator, 2 => worker, 3.. => other roles
-    uint[] skillIds;
-
-    // Maps a token to the sum of all payouts of it for this task
-    mapping (address => uint) totalPayouts;
-    // Maps task role ids (0,1,2..) to a token amount to be paid on task completion
-    mapping (uint => mapping (address => uint)) payouts;
-  }
-
-  struct Pot {
-    mapping (address => uint) balance;
-    uint taskId;
-  }
-
-  modifier taskExists(uint256 _id) {
-    require(_id <= taskCount);
-    _;
-  }
-
-  modifier taskNotAccepted(uint256 _id) {
-    require(!tasks[_id].accepted);
-    _;
-  }
-
-  modifier taskAccepted(uint256 _id) {
-    require(tasks[_id].accepted);
-    _;
-  }
-
-  modifier self() {
-    require(address(this) == msg.sender);
-    _;
-  }
-
-  modifier skillExists(uint256 _skillId){
-    IColonyNetwork colonyNetworkContract = IColonyNetwork(colonyNetworkAddress);
-    require(_skillId < colonyNetworkContract.skillCount());
-    _;
-  }
+contract Colony is ColonyStorage {
 
   function setToken(address _token) public
   auth
