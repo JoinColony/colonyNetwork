b3d82069d67036e0e241bf471bd818282d4b9169
elenadimitrova <elenavdimitrova@gmail.com>
2017-12-01 23:55:55 +0200

    Add the forwardTime helper function

--- helpers/test-helper.js
+++ helpers/test-helper.js
@@ -94,4 +94,12 @@ module.exports = {
   { 
     return Math.floor( Date.now() / 1000 ) ;
   },
+  forwardTime(seconds) {
+    console.log('Forwarding time with ' + seconds + 's ...');
+    return web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [seconds],
+      id: new Date().getTime() });
+  },
 };
