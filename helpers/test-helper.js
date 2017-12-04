1db601db3d3335a08d35182f74fb01e487e36d02
elenadimitrova <elenavdimitrova@gmail.com>
2017-12-04 13:07:30 +0200

    Call evm_mine after forwarding time

--- helpers/test-helper.js
+++ helpers/test-helper.js
@@ -96,10 +96,7 @@ module.exports = {
   },
   forwardTime(seconds) {
     console.log('Forwarding time with ' + seconds + 's ...');
-    return web3.currentProvider.send({
-      jsonrpc: '2.0',
-      method: 'evm_increaseTime',
-      params: [seconds],
-      id: new Date().getTime() });
-  },
+    web3.currentProvider.send({jsonrpc: "2.0", method: "evm_increaseTime", params: [seconds], id: 0});
+    web3.currentProvider.send({jsonrpc: "2.0", method: "evm_mine", params: [], id: 0});
+  }
 };
