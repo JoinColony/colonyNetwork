4d07cb817f611e6911ed2bf1e4ec457f50ffcf1c
elenadimitrova <elenavdimitrova@gmail.com>
2017-12-22 14:32:32 +0200

    Disable lint rule for parameter reassignment

--- helpers/upgradable-contracts.js
+++ helpers/upgradable-contracts.js
@@ -1,7 +1,6 @@
 import web3Utils from 'web3-utils';
-
-const assert = require('assert');
-const fs = require('fs');
+import assert from 'assert';
+import fs from 'fs';
 
 module.exports = {
   async setupUpgradableToken(token, resolver, etherRouter) {
@@ -41,15 +40,21 @@ module.exports = {
 
     // Load ABI of the interface of the contract we're trying to stich together
     const iAbi = JSON.parse(fs.readFileSync(`./build/contracts/${interfaceContract}.json`, 'utf8')).abi;
-    iAbi.map((value, index) => {
+    iAbi.map((value) => {
       const fName = value.name;
       const fType = value.type;
-      if (fName === 'authority' || fName === 'owner') { return; } // These are from DSAuth, and so are on EtherRouter itself without any more help.
-      if (fType !== 'function') { return; } // We only care about functions.
-      const fInputs = value.inputs.map(parameter => parameter.type); // Gets the types of the parameters, which is all we care about for function signatures.
-      const fOutputSize = value.outputs.length * 32;
-      // Record function name and how much data is returned
-      functionsToResolve[fName] = { inputs: fInputs, outputSize: fOutputSize, definedIn: '' };
+      // These are from DSAuth, and so are on EtherRouter itself without any more help.
+      if (fName !== 'authority' || fName !== 'owner') {
+        // We only care about functions.
+        if (fType === 'function') {
+          // Gets the types of the parameters, which is all we care about for function signatures.
+          const fInputs = value.inputs.map(parameter => parameter.type);
+          const fOutputSize = value.outputs.length * 32;
+          // Record function name and how much data is returned
+          functionsToResolve[fName] = { inputs: fInputs, outputSize: fOutputSize, definedIn: '' };
+        }
+      }
+      return functionsToResolve;
     });
 
     Object.keys(deployedImplementations).map(name => that.parseImplementation(name, functionsToResolve, deployedImplementations));
@@ -73,14 +78,18 @@ module.exports = {
       const fName = value.name;
       if (functionsToResolve[fName]) {
         if (functionsToResolve[fName].definedIn !== '') {
-          // It's a Friday afternoon, and I can't be bothered to deal with same name, different signature. Let's just resolve to not do it? We'd probably just
-          // trip ourselves up later.
+          // It's a Friday afternoon, and I can't be bothered to deal with same name, different signature.
+          // Let's just resolve to not do it? We'd probably just trip ourselves up later.
           // eslint-disable-next-line no-console
-          console.log('What are you doing defining functions with the same name in different files!? You are going to do yourself a mischief. You seem to have two ', fName, ' in ', contractName, 'and ', functionsToResolve[fName].definedIn);
+          console.log(
+            'What are you doing defining functions with the same name in different files!? You are going to do yourself a mischief. ',
+            'You seem to have two ', fName, ' in ', contractName, 'and ', functionsToResolve[fName].definedIn,
+          );
           process.exit(1);
         }
-        functionsToResolve[fName].definedIn = deployedImplementations[contractName];
+        functionsToResolve[fName].definedIn = deployedImplementations[contractName]; // eslint-disable-line no-param-reassign
       }
+      return functionsToResolve[fName];
     });
   },
 };
