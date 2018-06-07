9a87d016bb219625f82bd1c04144e12de171bebe
Christian Maniewski <chmanie@fastmail.com>
2018-06-07 09:13:55 +0200

    Revert truffle reporter change

--- truffle.js
+++ truffle.js
@@ -25,7 +25,7 @@ module.exports = {
     }
   },
   mocha: {
-    reporter: "eth-gas-reporter",
+    reporter: "mocha-circleci-reporter",
     reporterOptions: {
       currency: "USD",
       gasPrice: 5,
