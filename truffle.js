ff5993a416454c698b6721880e462a3663368fd5
elenadimitrova <elenavdimitrova@gmail.com>
2018-06-13 10:17:50 +0300

    Set gasLimit to be in line with ganache limit

--- truffle.js
+++ truffle.js
@@ -5,14 +5,14 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      gas: 7000000,
+      gas: 6700000,
       gasPrice: 0,
       network_id: "*"
     },
     integration: {
       host: "localhost",
       port: 8545,
-      gas: 7000000,
+      gas: 6700000,
       gasPrice: 0,
       network_id: "integration"
     },
