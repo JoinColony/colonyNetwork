5eab7fb9d8d2979b53f3ba4d06d6caee638bc3b8
James Lefrère <james@colony.io>
2018-06-15 14:21:22 +0200

    Fix version mapping
    * Specify the versions in the config file as an array (also specify the latest version)
    * Define versioned contracts by iterating through the config-defined versions
    * Separate versioned/static contracts constants
    * Look for static contracts first

--- packages/colony-contract-loader-network/src/NetworkLoader.js
+++ packages/colony-contract-loader-network/src/NetworkLoader.js
@@ -4,26 +4,31 @@ import ContractLoader, { truffleTransform } from "@colony/colony-js-contract-loa
 import config from "../config.json";
 
 const NETWORKS = ["rinkeby"];
-const LATEST_VERSION = config.LATEST_CONTRACT_VERSION;
-const VERSIONED_CONTRACTS = ["IColony", "IColonyNetwork", "Authority"];
-const STATIC_CONTRACTS = ["EtherRouter", "Token"];
+const { LATEST_VERSION, VERSIONS } = config;
+const VERSIONED_CONTRACT_NAMES = ["IColony", "IColonyNetwork", "Authority"];
+const STATIC_CONTRACT_NAMES = ["EtherRouter", "Token"];
 
-const CONTRACTS = {};
+const STATIC_CONTRACTS = Object.assign(
+  {},
+  ...STATIC_CONTRACT_NAMES.map(contract => ({
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    [contract]: require(`../contracts/static/${contract}.json`)
+  }))
+);
+
+const VERSIONED_CONTRACTS = {};
+// Define versioned contracts
 NETWORKS.forEach(network => {
-  [...Array(LATEST_VERSION).keys()].forEach(version => {
-    VERSIONED_CONTRACTS.forEach(contract => {
-      CONTRACTS[contract] = {};
-      if (!CONTRACTS[contract][network]) {
-        CONTRACTS[contract][network] = {};
+  VERSIONS.forEach(version => {
+    VERSIONED_CONTRACT_NAMES.forEach(contract => {
+      VERSIONED_CONTRACTS[contract] = {};
+      if (!VERSIONED_CONTRACTS[contract][network]) {
+        VERSIONED_CONTRACTS[contract][network] = {};
       }
       // eslint-disable-next-line global-require, import/no-dynamic-require
-      CONTRACTS[contract][network][version + 1] = require(`../contracts/versioned/${network}-v${version + 1}/${contract}.json`);
+      VERSIONED_CONTRACTS[contract][network][version] = require(`../contracts/versioned/${network}-v${version}/${contract}.json`);
     });
   });
-});
-STATIC_CONTRACTS.forEach(contract => {
-  // eslint-disable-next-line global-require, import/no-dynamic-require
-  CONTRACTS[contract] = require(`../contracts/static/${contract}.json`);
 });
 
 class NetworkLoader extends ContractLoader {
@@ -37,10 +42,14 @@ class NetworkLoader extends ContractLoader {
     assert(!!contractName, "A `contractName` option must be provided");
     assert(!!version, "A valid `version` option must be provided");
 
-    if (CONTRACTS[contractName] && CONTRACTS[contractName][this._network] && CONTRACTS[contractName][this._network][version]) {
-      return this._transform(CONTRACTS[contractName][this._network][version], query, requiredProps);
-    } else if (CONTRACTS[contractName]) {
-      return this._transform(CONTRACTS[contractName], query, requiredProps);
+    if (STATIC_CONTRACTS[contractName]) {
+      return this._transform(STATIC_CONTRACTS[contractName], query, requiredProps);
+    } else if (
+      VERSIONED_CONTRACTS[contractName] &&
+      VERSIONED_CONTRACTS[contractName][this._network] &&
+      VERSIONED_CONTRACTS[contractName][this._network][version]
+    ) {
+      return this._transform(VERSIONED_CONTRACTS[contractName][this._network][version], query, requiredProps);
     }
     throw new Error(`Contract ${contractName} with version ${version} not found in ${this._network}`);
   }
