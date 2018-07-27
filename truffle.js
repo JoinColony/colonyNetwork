6f4cfdec6e9c941b70b066050cab65aa422ef754
elenadimitrova <elenavdimitrova@gmail.com>
2018-07-27 15:36:21 +0300

    Remove websockets config from truffle

--- truffle.js
+++ truffle.js
@@ -12,16 +12,14 @@ module.exports = {
       host: "localhost",
       port: 8545,
       gasPrice: 0,
-      network_id: "integration",
-      websockets: true
+      network_id: "integration"
     },
     coverage: {
       host: "localhost",
       network_id: "*",
       port: 8555, // <-- Use port 8555
       gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01, // <-- Use this low gas price
-      websockets: true
+      gasPrice: 0x01 // <-- Use this low gas price
     }
   },
   mocha: {
