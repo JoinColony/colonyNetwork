e221d732a3023c0f2a795161a67074337fed0d05
Daniel Kronovet <kronovet@gmail.com>
2018-11-30 15:00:46 +0000

    Reorganize constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,24 +1,34 @@
 import { toBN, soliditySha3 } from "web3-utils";
 import shortid from "shortid";
 
+const UINT256_MAX = toBN(0).notn(256);
+const INT256_MAX = toBN(0).notn(255);
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
 const MANAGER_ROLE = 0;
 const EVALUATOR_ROLE = 1;
 const WORKER_ROLE = 2;
+
 // The base58 decoded, bytes32 converted hex value of a test task ipfsHash "QmNSUYVKDSvPUnRLKmuxk9diJ6yS96r1TrAXzjTiBcCLAL"
 const SPECIFICATION_HASH = "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231";
 // The above bytes32 hash where the last raw byte was changed from 1 -> 2
 const SPECIFICATION_HASH_UPDATED = "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f232";
 // The base58 decoded, bytes32 converted hex value of a test task ipfsHash "qmv8ndh7ageh9b24zngaextmuhj7aiuw3scc8hkczvjkww"
 const DELIVERABLE_HASH = "0xfb027a4d64f29d83e27769cb05d945e67ef7396fa1bd73ef53f065311fd3313e";
-const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-const INITIAL_FUNDING = toBN(360 * 1e18);
-const MANAGER_PAYOUT = toBN(100 * 1e18);
-const EVALUATOR_PAYOUT = toBN(50 * 1e18);
-const WORKER_PAYOUT = toBN(200 * 1e18);
+
+const WAD = toBN(10).pow(toBN(18));
+const MIN_STAKE = WAD.muln(2000);
+const DEFAULT_STAKE = MIN_STAKE.muln(1000);
+
+const INITIAL_FUNDING = WAD.muln(360);
+const MANAGER_PAYOUT = WAD.muln(100);
+const EVALUATOR_PAYOUT = WAD.muln(50);
+const WORKER_PAYOUT = WAD.muln(200);
+
 const MANAGER_RATING = 2;
 const WORKER_RATING = 3;
 const RATING_MULTIPLIER = { 1: -1, 2: 1, 3: 1.5 };
-const SECONDS_PER_DAY = 86400;
+
 const RATING_1_SALT = soliditySha3(shortid.generate());
 const RATING_2_SALT = soliditySha3(shortid.generate());
 const RATING_1_SECRET = soliditySha3(RATING_1_SALT, MANAGER_RATING);
@@ -28,13 +38,7 @@ const ACTIVE_TASK_STATE = 0;
 const CANCELLED_TASK_STATE = 1;
 const FINALIZED_TASK_STATE = 2;
 
-const UINT256_MAX = toBN(0).notn(256);
-const INT256_MAX = toBN(0).notn(255);
-
-const WAD = toBN(10).pow(toBN(18));
-const MIN_STAKE = WAD.muln(2000);
-const DEFAULT_STAKE = MIN_STAKE.muln(1000);
-
+const SECONDS_PER_DAY = 86400;
 const MINING_CYCLE_DURATION = 60 * 60 * 24; // 24 hours
 const DECAY_RATE = {
   NUMERATOR:    toBN("992327946262944"), // eslint-disable-line prettier/prettier
@@ -42,13 +46,18 @@ const DECAY_RATE = {
 };
 
 module.exports = {
+  UINT256_MAX,
+  INT256_MAX,
+  ZERO_ADDRESS,
   MANAGER_ROLE,
   EVALUATOR_ROLE,
   WORKER_ROLE,
   SPECIFICATION_HASH,
   SPECIFICATION_HASH_UPDATED,
   DELIVERABLE_HASH,
-  SECONDS_PER_DAY,
+  WAD,
+  MIN_STAKE,
+  DEFAULT_STAKE,
   INITIAL_FUNDING,
   MANAGER_PAYOUT,
   EVALUATOR_PAYOUT,
@@ -63,12 +72,7 @@ module.exports = {
   ACTIVE_TASK_STATE,
   CANCELLED_TASK_STATE,
   FINALIZED_TASK_STATE,
-  UINT256_MAX,
-  INT256_MAX,
-  WAD,
-  MIN_STAKE,
-  DEFAULT_STAKE,
+  SECONDS_PER_DAY,
   MINING_CYCLE_DURATION,
-  DECAY_RATE,
-  ZERO_ADDRESS
+  DECAY_RATE
 };
