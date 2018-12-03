8ea95217108d5f8ef1f7dca48fc7f971fc56c372
elenadimitrova <elenavdimitrova@gmail.com>
2018-12-03 19:56:59 +0200

    Make ganache-cli generate 12 test accounts

--- .solcover.js
+++ .solcover.js
@@ -8,5 +8,5 @@ module.exports = {
     ],
     compileCommand: '../node_modules/.bin/truffle compile',
     testCommand: '../node_modules/.bin/truffle test --network coverage',
-    testrpcOptions: `--port 8555 -i 1999 --acctKeys="./coverageEnv/ganache-accounts.json" --noVMErrorsOnRPCResponse`
+    testrpcOptions: `--port 8555 -i 1999 --acctKeys="./coverageEnv/ganache-accounts.json" --noVMErrorsOnRPCResponse --accounts 12`
 };
