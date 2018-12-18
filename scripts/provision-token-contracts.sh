1d408c52604b58284848e1017b981cdb45ba229f
elenadimitrova <elenavdimitrova@gmail.com>
2018-12-18 12:44:53 +0200

    Amend script to provision folder if nonexistent

--- scripts/provision-token-contracts.sh
+++ scripts/provision-token-contracts.sh
@@ -3,6 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
 
+test ! -d ./build/contracts/ && mkdir -p ./build/contracts/
+
 cp lib/colonyToken/build/contracts/Token.json ./build/contracts/Token.json
 cp lib/colonyToken/build/contracts/TokenAuthority.json ./build/contracts/TokenAuthority.json
 cp lib/colonyToken/build/contracts/MultiSigWallet.json ./build/contracts/MultiSigWallet.json
