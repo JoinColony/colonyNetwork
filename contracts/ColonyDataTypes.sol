063ba42e5ec2ec6585b0ed9f279d14bba0efed7c
elenadimitrova <elenavdimitrova@gmail.com>
2018-12-20 17:10:57 +0200

    Switch pragma back to supporting 0.4.23 and after solc version

--- contracts/ColonyDataTypes.sol
+++ contracts/ColonyDataTypes.sol
@@ -15,7 +15,7 @@
   along with The Colony Network. If not, see <http://www.gnu.org/licenses/>.
 */
 
-pragma solidity >0.5.0;
+pragma solidity >=0.4.23;
 
 
 contract ColonyDataTypes {
--- contracts/PatriciaTree/Data.sol
+++ contracts/PatriciaTree/Data.sol
@@ -1,4 +1,4 @@
-pragma solidity >0.5.0;
+pragma solidity >=0.4.23;
 pragma experimental "ABIEncoderV2";
 
 import {Bits} from "./Bits.sol";
