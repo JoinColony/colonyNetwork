22b71014d8ca6b25f570028f7e29c845777bcdcf
elenadimitrova <elenavdimitrova@gmail.com>
2018-12-29 17:29:17 +0200

    Copy openzeppelin-solidity package into SC env

--- .solcover.js
+++ .solcover.js
@@ -5,6 +5,7 @@ module.exports = {
       'PatriciaTree',
       'ERC20ExtendedToken.sol'
     ],
+    copyPackages: ['openzeppelin-solidity'],
     compileCommand: 'yarn run provision:token:contracts:compile',
     testCommand: '../node_modules/.bin/truffle test --network coverage',
     testrpcOptions: `--port 8555 -i 1999 --acctKeys="./coverageEnv/ganache-accounts.json" --noVMErrorsOnRPCResponse --accounts 12`
