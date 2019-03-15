7dcdece525e599cd114af9676dd56498ef135e27
Alex <git@area.io>
2019-03-15 15:46:38 +0000

    Bump NoLimitSubdomains to solidity >=0.5.3

--- contracts/testHelpers/NoLimitSubdomains.sol
+++ contracts/testHelpers/NoLimitSubdomains.sol
@@ -15,7 +15,7 @@
   along with The Colony Network. If not, see <http://www.gnu.org/licenses/>.
 */
 
-pragma solidity >=0.4.23 <0.5.0;
+pragma solidity >=0.5.3;
 pragma experimental ABIEncoderV2;
 
 import "./../ColonyStorage.sol";
