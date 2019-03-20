4bb29b1c1c7312a4fb62feb0fd304d2f170483dc
elenadimitrova <elenavdimitrova@gmail.com>
2019-03-20 15:11:55 +0000

    Remove leftover .only on test

--- test/colony-payment.js
+++ test/colony-payment.js
@@ -14,7 +14,7 @@ const EtherRouter = artifacts.require("EtherRouter");
 const IColonyNetwork = artifacts.require("IColonyNetwork");
 const DSToken = artifacts.require("DSToken");
 
-contract.only("Colony Payment", accounts => {
+contract("Colony Payment", accounts => {
   const RECIPIENT = accounts[3];
   const COLONY_ADMIN = accounts[4];
 
