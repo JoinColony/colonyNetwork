93a63a35e1e36cedd86e2024d06d02633db1e414
elenadimitrova <elenavdimitrova@gmail.com>
2019-05-20 13:19:02 +0300

    Use the correct higher order helper function in test

--- test/contracts-network/colony-task.js
+++ test/contracts-network/colony-task.js
@@ -30,7 +30,7 @@ import {
   GLOBAL_SKILL_ID
 } from "../../helpers/constants";
 
-import { createSignatures, getSigsAndTransactionData, executeSignedTaskChange, executeSignedRoleAssignment } from "../helpers/task-review-signing";
+import { getSigsAndTransactionData, executeSignedTaskChange, executeSignedRoleAssignment } from "../helpers/task-review-signing";
 
 import {
   getTokenArgs,
@@ -220,15 +220,15 @@ contract("ColonyTask", accounts => {
 
       const sigTypes = [0, 0];
       const signers = [MANAGER, WORKER];
-      // We have to pass in an ethers BN because of https://github.com/ethereum/web3.js/issues/1920
-      const ethersBN = ethers.utils.bigNumberify(taskId.toString());
-      const txData = await colony.contract.methods.setTaskWorkerRole(ethersBN, WORKER).encodeABI();
-      const sigsPromises = sigTypes.map((type, i) => createSignatures(colony, taskId, [signers[i]], 0, txData));
-      const sigs = await Promise.all(sigsPromises);
-      const sigV = sigs.map(sig => sig.sigV[0]);
-      const sigR = sigs.map(sig => sig.sigR[0]);
-      const sigS = sigs.map(sig => sig.sigS[0]);
-
+      const args = [taskId, WORKER];
+      const { sigV, sigR, sigS, txData } = await getSigsAndTransactionData({
+        colony,
+        taskId,
+        functionName: "setTaskWorkerRole",
+        signers,
+        sigTypes,
+        args
+      });
       await checkErrorRevert(colony.executeTaskRoleAssignment(sigV, sigR, sigS, sigTypes, 10, txData), "colony-task-role-assignment-non-zero-value");
     });
 
