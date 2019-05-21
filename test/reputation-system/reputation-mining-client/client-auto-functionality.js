bd8723564deece84623180abd59a89f512d0d3f1
Alex <git@area.io>
2019-05-21 16:02:43 +0100

    Bring auto tests under the testing umbrella

--- test/reputation-system/reputation-mining-client/client-auto-functionality.js
+++ test/reputation-system/reputation-mining-client/client-auto-functionality.js
@@ -6,7 +6,7 @@ import bnChai from "bn-chai";
 
 import { TruffleLoader } from "@colony/colony-js-contract-loader-fs";
 
-import { DEFAULT_STAKE, MINING_CYCLE_DURATION } from "../../helpers/constants";
+import { DEFAULT_STAKE, MINING_CYCLE_DURATION } from "../../../helpers/constants";
 import {
   getActiveRepCycle,
   forwardTime,
@@ -16,23 +16,23 @@ import {
   mineBlock,
   finishReputationMiningCycle,
   currentBlock
-} from "../../helpers/test-helper";
+} from "../../../helpers/test-helper";
 import {
   setupColonyNetwork,
   setupMetaColonyWithLockedCLNYToken,
   giveUserCLNYTokensAndStake,
   setupFinalizedTask,
   fundColonyWithTokens
-} from "../../helpers/test-data-generator";
-import ReputationMinerClient from "../../packages/reputation-miner/ReputationMinerClient";
-import ReputationMinerTestWrapper from "../../packages/reputation-miner/test/ReputationMinerTestWrapper";
-import MaliciousReputationMinerExtraRep from "../../packages/reputation-miner/test/MaliciousReputationMinerExtraRep";
+} from "../../../helpers/test-data-generator";
+import ReputationMinerClient from "../../../packages/reputation-miner/ReputationMinerClient";
+import ReputationMinerTestWrapper from "../../../packages/reputation-miner/test/ReputationMinerTestWrapper";
+import MaliciousReputationMinerExtraRep from "../../../packages/reputation-miner/test/MaliciousReputationMinerExtraRep";
 
 const { expect } = chai;
 chai.use(bnChai(web3.utils.BN));
 
 const loader = new TruffleLoader({
-  contractDir: path.resolve(__dirname, "..", "..", "build", "contracts")
+  contractDir: path.resolve(__dirname, "..", "..", "..", "build", "contracts")
 });
 
 const realProviderPort = process.env.SOLIDITY_COVERAGE ? 8555 : 8545;
@@ -388,6 +388,58 @@ process.env.SOLIDITY_COVERAGE
           // And finally, check the root hash was accepted as expected.
           const acceptedRootHash = await colonyNetwork.getReputationRootHash();
           assert.equal(acceptedRootHash, rootHash);
+        });
+
+        it.skip("should cope if not processing anything and a new cycle starts", async function() {
+          await reputationMinerClient.close();
+          await goodClient.addLogContentsToReputationTree();
+          await goodClient.saveCurrentState();
+          await forwardTime(MINING_CYCLE_DURATION / 2, this);
+          await goodClient.submitRootHash();
+          console.log("submitted");
+          await forwardTime(MINING_CYCLE_DURATION, this);
+          console.log("forard");
+          const repCycleEthers = await reputationMinerClient._miner.getActiveRepCycle();
+          const receive12Submissions = new Promise(function(resolve, reject) {
+            repCycleEthers.on("ReputationRootHashSubmitted", async (_miner, _hash, _nNodes, _jrh, _entryIndex, event) => {
+              const nSubmissions = await repCycle.getNSubmissionsForHash(_hash, _nNodes, _jrh);
+
+              if (nSubmissions.toNumber() === 12) {
+                // Check the reputation cycle submission matches our miners
+                for (let i = 0; i < 12; i += 1) {
+                  // Validate the miner address in submission is correct
+                  const submitter = await repCycle.getSubmissionUser(_hash, _nNodes, _jrh, i);
+                  expect(submitter).to.be.oneOf([MINER1, MINER2]);
+                }
+
+                // Validate the root hash matches what the miners submitted
+                const submission = await repCycle.getReputationHashSubmission(MINER1);
+                expect(submission.proposedNewRootHash).to.equal(_hash);
+                const submission2 = await repCycle.getReputationHashSubmission(MINER2);
+                expect(submission2.proposedNewRootHash).to.equal(_hash);
+
+                event.removeListener();
+                resolve();
+              } else {
+                await mineBlock();
+              }
+            });
+
+            // After 30s, we throw a timeout error
+            setTimeout(() => {
+              reject(new Error("ERROR: timeout while waiting for 12 hash submissions"));
+            }, 30000);
+          });
+
+          await reputationMinerClient.initialise(colonyNetwork.address, startingBlockNumber);
+          // const x = new Promise((resolve, reject) => {})
+          console.log("confirm");
+          // await x
+          await repCycleEthers.confirmNewHash(1);
+          console.log("confirmed");
+          await forwardTime(MINING_CYCLE_DURATION / 2, this);
+
+          await receive12Submissions;
         });
       });
     });
