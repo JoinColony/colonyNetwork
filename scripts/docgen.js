55beb2adc7355cffaf716f68d5b3922c8b2ce0d8
Ryan Christoffersen <ryan@ryanchristo.com>
2019-05-23 18:17:30 +0200

    Find and parse natspec comments

--- scripts/docgen.js
+++ scripts/docgen.js
@@ -50,11 +50,96 @@ const generateMarkdown = ({ contractFile, templateFile, outputFile }) => {
 
   const ast = parser.parse(contractFileString);
 
+  const contractFileArray = contractFileString.split('\n');
   const contract = ast.children.find(child => child.type === 'ContractDefinition');
+
+  const methods = [];
+
+  contract.subNodes.map(method => {
+
+    // Set the maximum number of lines we want to check
+    const maxLines = 10;
+
+    // Set the intial value of notice
+    let notice;
+
+    // Set the intial value of params
+    const params = [];
+
+    // Set the intial value of returnParams
+    const returnParams = [];
+
+    // Set the methodLineIndex
+    const methodLineIndex = contractFileArray.findIndex(line => {
+      return line.includes(`function ${method.name}`)
+    });
+
+    // Set the initial value of noticeLineIndex
+    let noticeLineIndex = methodLineIndex;
+
+    // Find the noticeLineIndex with a maximum number of lines to check
+    while(!contractFileArray[noticeLineIndex].includes( '@notice ')) {
+      if (methodLineIndex - maxLines === noticeLineIndex) {
+        noticeLineIndex = 0;
+        return;
+      }
+      noticeLineIndex -= 1;
+    }
+
+    // Update notice value
+    if (noticeLineIndex) {
+      notice = contractFileArray[noticeLineIndex].split(' @notice ')[1];
+    }
+
+    // Set the initial value of paramLineIndex
+    let paramLineIndex = noticeLineIndex;
+
+    if (method.parameters) {
+      // Find the params with a maximum number of lines to check
+      while (paramLineIndex && params.length !== method.parameters.parameters.length) {
+        if (noticeLineIndex + maxLines === paramLineIndex) {
+          paramLineIndex = 0;
+          return;
+        }
+        if (contractFileArray[paramLineIndex].includes(' @param ')) {
+          params.push(contractFileArray[paramLineIndex].split(' @param ')[1]);
+        }
+        paramLineIndex += 1;
+      }
+    }
+
+    // Set the initial value of paramLineIndex
+    let returnParamLineIndex = noticeLineIndex;
+
+    if (method.returnParameters) {
+      // Find the return params with a maximum number of lines to check
+      while (returnParamLineIndex && returnParams.length !== method.returnParameters.parameters.length) {
+        if (noticeLineIndex + maxLines === returnParamLineIndex) {
+          returnParamLineIndex = 0;
+          return;
+        }
+        if (contractFileArray[returnParamLineIndex].includes(' @return ')) {
+          returnParams.push(contractFileArray[returnParamLineIndex].split(' @return ')[1]);
+        }
+        returnParamLineIndex += 1;
+      }
+    }
+
+    // Push method to methods array
+    methods.push({
+      ...method,
+      natspec: {
+        notice,
+        params,
+        returnParams,
+      },
+    });
+
+  });
 
   const md = `
   ${templateFileString}
-  ${printMethods(contract.subNodes)}
+  ${printMethods(methods)}
   `.trim();
 
   fs.writeFileSync(outputFile, md);
@@ -79,27 +164,36 @@ function printMethods(methods) {
 
 ### \`${method.name}\`
 
+${method.natspec.notice}
+
 ${method.parameters && method.parameters.parameters.length ? `
 
 **Parameters**
 
-` + printParams(method.parameters.parameters) : ''}
+` + printParams(method.parameters.parameters, method.natspec.params) : ''}
 
 ${method.returnParameters && method.returnParameters.parameters.length ? `
 
 **Return Parameters**
 
-` + printParams(method.returnParameters.parameters) : ''}
+` + printParams(method.returnParameters.parameters, method.natspec.returnParams) : ''}
 
 `).join('');
 
 }
 
-function printParams(params) {
+function printParams(params, natspecParams) {
   if (params && params.length) return `
-|Name|Type|
+|Name|Type|Description|
 |---|---|---|
 ${params
-  .map(param => `|${param.name}|${param.typeName.name}|`)
+  .map((param, index) => {
+    const noName = '_undefined_';
+    const name = param.name ? param.name : noName;
+    const type = param.typeName.name;
+    const valid = natspecParams[index].substring(0, name.length) === name
+    const description = valid ? natspecParams[index].split(name, 2)[1] : '';
+    return `|${name}|${type || 'memory' }|${description}`
+  })
   .join('\n')}`;
 }
