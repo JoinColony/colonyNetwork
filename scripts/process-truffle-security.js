7019200a818b8a7de3d8c57c3e55af0188d6f46b
elenadimitrova <elenavdimitrova@gmail.com>
2019-06-14 16:24:00 +0300

    Wrap the security warning parsing into a try/catch

--- scripts/process-truffle-security.js
+++ scripts/process-truffle-security.js
@@ -1,8 +1,8 @@
 const nthline = require("nthline");
 // This file doesn't exist unless we've run the truffle security package of tests, so we don't expect
 // it to exist while running eslint in CI
-const results = require("../truffle-security-output.json"); // eslint-disable-line import/no-unresolved
-
+const results = require("../truffle-security-output.json");
+// eslint-disable-line import/no-unresolved
 let fail = false;
 
 async function main() {
@@ -11,11 +11,15 @@ async function main() {
     for (let j = 0; j < file.messages.length; j += 1) {
       const message = file.messages[j];
       // load relevant line from file
-      const line = await nthline(message.line - 1, file.filePath);
-      if (line.toLowerCase().search(new RegExp(`//.*ignore-${message.ruleId.toLowerCase()}`, "g")) === -1) {
-        fail = true;
-        console.log("Failing file: ", file.filePath);
-        console.log(message);
+      try {
+        const line = await nthline(message.line - 1, file.filePath);
+        if (line.toLowerCase().search(new RegExp(`//.*ignore-${message.ruleId.toLowerCase()}`, "g")) === -1) {
+          fail = true;
+          console.log("Failing file: ", file.filePath);
+          console.log(message);
+        }
+      } catch (err) {
+        console.log("ERROR:", err);
       }
     }
   }
