ffb5c5440ce6a47b1630fbe2bd327cde91988c71
erak <erik@zirkular.io>
2019-10-31 13:33:32 +0100

    Updates contracts to Solidity 0.6.0

--- contracts/EtherRouter.sol
+++ contracts/EtherRouter.sol
@@ -51,13 +51,13 @@ contract EtherRouter is DSAuth {
       let size := extcodesize(destination)
       if eq(size, 0) { revert(0,0) }
 
-      calldatacopy(mload(0x40), 0, calldatasize)
-      let result := delegatecall(gas, destination, mload(0x40), calldatasize, mload(0x40), 0) // ignore-swc-113
+      calldatacopy(mload(0x40), 0, calldatasize())
+      let result := delegatecall(gas(), destination, mload(0x40), calldatasize(), mload(0x40), 0) // ignore-swc-113
       // as their addresses are controlled by the Resolver which we trust
-      returndatacopy(mload(0x40), 0, returndatasize)
+      returndatacopy(mload(0x40), 0, returndatasize())
       switch result
-      case 1 { return(mload(0x40), returndatasize) }
-      default { revert(mload(0x40), returndatasize) }
+      case 1 { return(mload(0x40), returndatasize()) }
+      default { revert(mload(0x40), returndatasize()) }
     }
   }
 
