37d85b63ca31147b921154681d0e97ff21217157
Alex <git@area.io>
2021-01-18 12:03:04 +0000

    Try commenting out unneeded files in migration

--- scripts/deployUnmockedColonyVersions.js
+++ scripts/deployUnmockedColonyVersions.js
@@ -18,30 +18,39 @@ const IMetaColony = artifacts.require("./IMetaColony");
 module.exports = async () => {
   // While debugging, always checkout currenthash
   // await exec("git checkout 2690fba3d3002fd72912cd74f1fbf4932c734e94 && git submodule update");
-  console.log('last chance')
-  let currentHash = await exec("git log -1 --format='%H'");
+  const currentHash = await exec("git log -1 --format='%H'");
   // const currentHash = res.stdout.trim();
-  console.log(currentHash, 'hash')
   let v3ResolverAddress;
   let v4ResolverAddress;
-  console.log('maybe here?')
   const colonyNetwork = await IColonyNetwork.at(cnAddress);
-  console.log(cnAddress, 'cnAddress');
   const metaColonyAddress = await colonyNetwork.getMetaColony();
   const metaColony = await IMetaColony.at(metaColonyAddress);
-  console.log('here')
   try {
     await exec("git checkout ad5569de24567517aa12624e29600c9136fb594d && git submodule update");
 
-    let res = await exec("yarn run truffle migrate --reset");
+    // Comment out uneeded parts of file
+    await exec("sed -i'' '26,27 s|^|//|' ./migrations/4_setup_colony_version_resolver.js");
+    await exec("sed -i'' '31 s|^|//|' ./migrations/4_setup_colony_version_resolver.js");
+    let res = await exec("yarn run truffle migrate --reset -f 4 --to 4");
+    if (res.stdout) {
+      // How this response looks changes node 10->12
+      res = res.stdout;
+    }
     let index = res.indexOf("Colony version 3 set to Resolver");
     v3ResolverAddress = res.substring(index + 33, index + 33 + 42);
     console.log("v3 address:", v3ResolverAddress);
-    await exec("git checkout burgundy-glider && git submodule update");
-
     await metaColony.addNetworkColonyVersion(3, v3ResolverAddress);
 
-    res = await exec("yarn run truffle migrate --reset");
+    await exec("git checkout -f burgundy-glider && git submodule update");
+
+    // Comment out uneeded parts of file
+    await exec("sed -i'' '27,28 s|^|//|' ./migrations/4_setup_colony_version_resolver.js");
+    await exec("sed -i'' '32 s|^|//|' ./migrations/4_setup_colony_version_resolver.js");
+    res = await exec("yarn run truffle migrate --reset -f 4 --to 4");
+    if (res.stdout) {
+      // How this response looks changes node 10->12
+      res = res.stdout;
+    }
     index = res.indexOf("Colony version 4 set to Resolver");
     v4ResolverAddress = res.substring(index + 33, index + 33 + 42);
     await metaColony.addNetworkColonyVersion(4, v4ResolverAddress);
@@ -51,7 +60,7 @@ module.exports = async () => {
   console.log(v3ResolverAddress);
   console.log(v4ResolverAddress);
   // put things back how they were.
-  await exec(`git checkout ${currentHash}`);
+  await exec(`git checkout -f ${currentHash}`);
   await exec("git submodule update");
   await exec("yarn run truffle compile");
 };
