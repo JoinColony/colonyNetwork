a960490bb4201c0414da51e1d5af05cee0e41cd4
Alex <git@area.io>
2021-02-04 09:48:19 +0000

    Use existing config for chainid coverage tests

--- .solcover.chainid.js
+++ .solcover.chainid.js
@@ -1,35 +1,8 @@
-const { execSync } = require("child_process");
-const log = console.log;
+const config = require("./.solcover.js")
 
-// Copies pre-built token artifacts to .coverage_artifacts/contracts
-function provisionTokenContracts(config){
-  let output;
-  const provisionColonyToken = `bash ./scripts/provision-token-contracts.sh`;
+config.providerOptions.network_id = parseInt(process.env.CHAIN_ID, 10);
+config.providerOptions._chainId = parseInt(process.env.CHAIN_ID, 10);
+config.providerOptions._chainIdRpc = parseInt(process.env.CHAIN_ID, 10);
+config.istanbulFolder = `./coverage-chainid-${process.env.CHAIN_ID}`
 
-  log('Provisioning ColonyToken contracts...')
-  output = execSync(provisionColonyToken);
-  log(output.toString())
-}
-
-module.exports = {
-    skipFiles: [
-      'Migrations.sol',
-      'common/EtherRouter.sol',
-      'patriciaTree',
-      'testHelpers/ContractEditing.sol', // only used in setting up colony-network-recovery.js tests, never in production
-      'testHelpers/NoLimitSubdomains.sol',
-      'testHelpers/TaskSkillEditing.sol'
-    ],
-    providerOptions: {
-      port: 8555,
-      network_id: parseInt(process.env.CHAIN_ID, 10),
-      _chainId: parseInt(process.env.CHAIN_ID, 10),
-      _chainIdRpc: parseInt(process.env.CHAIN_ID, 10),
-      account_keys_path: "./ganache-accounts.json",
-      vmErrorsOnRPCResponse: false,
-      total_accounts: 18
-    },
-    onCompileComplete: provisionTokenContracts,
-    istanbulFolder: `./coverage-chainid-${process.env.CHAIN_ID}`
-}
-
+module.exports = config
