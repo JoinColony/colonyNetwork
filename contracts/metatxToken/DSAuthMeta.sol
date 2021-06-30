c811aab7042c02a2636e7bc71303c4022988751e
Alex Rea <git@area.io>
2021-06-30 11:18:01 +0100

    Split out contracts that were in metatxtoken.sol

---
+++ contracts/metatxToken/DSAuthMeta.sol
@@ -0,0 +1,61 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import "./../common/ERC20Extended.sol";
+import "./../common/BasicMetaTransaction.sol";
+import "./../common/ERC20Extended.sol";
+import "./../../lib/dappsys/auth.sol";
+
+pragma solidity 0.7.3;
+
+abstract contract DSAuthMeta is DSAuthEvents, BasicMetaTransaction {
+    DSAuthority  public  authority;
+    address      public  owner;
+
+    constructor() {
+        owner = msgSender();
+        emit LogSetOwner(msgSender());
+    }
+
+    function setOwner(address owner_)
+        public
+        auth
+    {
+        owner = owner_;
+        emit LogSetOwner(owner);
+    }
+
+    function setAuthority(DSAuthority authority_)
+        public
+        auth
+    {
+        authority = authority_;
+        emit LogSetAuthority(address(authority));
+    }
+
+    modifier auth {
+        require(isAuthorized(msgSender(), msg.sig), "ds-auth-unauthorized");
+        _;
+    }
+
+    function isAuthorized(address src, bytes4 sig) internal view returns (bool) {
+        if (src == address(this)) {
+            return true;
+        } else if (src == owner) {
+            return true;
+        } else if (authority == DSAuthority(0)) {
+            return false;
+        } else {
+            return authority.canCall(src, address(this), sig);
+        }
+    }
+}
