9a5b1bbb280d5599b8395afd6cf4c82a8907edce
Alex Rea <git@area.io>
2021-12-06 10:11:52 +0000

    Change how CoinMachine limits are enforced

--- test-smoke/colony-storage-consistent.js
+++ test-smoke/colony-storage-consistent.js
@@ -154,7 +154,7 @@ contract("Contract Storage", (accounts) => {
       console.log("miningCycleStateHash:", miningCycleAccount.stateRoot.toString("hex"));
       console.log("tokenLockingStateHash:", tokenLockingAccount.stateRoot.toString("hex"));
 
-      expect(colonyNetworkAccount.stateRoot.toString("hex")).to.equal("6b2b49d0511c9ba1050df1531f86df8d9f96075d59e736fea51235f011e6865a");
+      expect(colonyNetworkAccount.stateRoot.toString("hex")).to.equal("197678aa163dbee5ac0282328d4c46c371d879e823bfd4bb33cb69d47c19cb4a");
       expect(colonyAccount.stateRoot.toString("hex")).to.equal("d8e895aa214956a2543325209231d4502c6c241027df357ce9bcf065215c4632");
       expect(metaColonyAccount.stateRoot.toString("hex")).to.equal("78294685a492256887e3159e26071ba06d62883c72ccb26fd323a883eefc30fd");
       expect(miningCycleAccount.stateRoot.toString("hex")).to.equal("e105190bcd647989da1579ac209ae54ed63e08224fbb2469bad9f596773fe558");
