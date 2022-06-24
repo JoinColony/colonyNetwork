63f4dcf41886a0746681ba8b929ea8d082c9c595
chmanie <code@chmanie.com>
2022-06-24 12:09:07 +0200

    Make `createClaimDelayAction internal

--- contracts/extensions/votingReputation/VotingReputation.sol
+++ contracts/extensions/votingReputation/VotingReputation.sol
@@ -1083,7 +1083,7 @@ contract VotingReputation is ColonyExtension, PatriciaTreeProofs, BasicMetaTrans
   }
 
   function createClaimDelayAction(bytes memory action, uint256 value)
-    public
+    internal
     returns (bytes memory)
   {
     // See https://solidity.readthedocs.io/en/develop/abi-spec.html#use-of-dynamic-types
