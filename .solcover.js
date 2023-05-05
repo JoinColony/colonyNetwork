bf8b52286f44b6057bce9a0b0439d0e81711d009
Alex Rea <git@area.io>
2023-05-05 10:47:49 +0100

    Additional multichain tests

--- .solcover.js
+++ .solcover.js
@@ -59,5 +59,5 @@ module.exports = {
     },
     onCompileComplete: provisionTokenContracts,
     istanbulFolder: "./coverage-contracts",
-    modifierWhitelist: ["always"],
+    modifierWhitelist: ["always", "onlyMiningChain", "onlyNotMiningChain"],
 }
