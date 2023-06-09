3654b20f508ee2fe0a12f49229e01ba8547f2036
Alex Rea <git@area.io>
2023-06-09 22:52:58 +0100

    First changes following second review

--- contracts/colony/ColonyAuthority.sol
+++ contracts/colony/ColonyAuthority.sol
@@ -131,7 +131,7 @@ contract ColonyAuthority is CommonAuthority {
     addRoleCapability(ARBITRATION_ROLE, "setExpenditurePayout(uint256,uint256,uint256,uint256,address,uint256)");
 
     // Added in colony vxxx
-    addRoleCapability(ROOT_ROLE, "setBridgeData(address,bytes,bytes,uint256,uint256,bytes,bytes,bytes,bytes)");
+    addRoleCapability(ROOT_ROLE, "setBridgeData(address,uint256,uint256,bytes,bytes,bytes,bytes,bytes,bytes)");
   }
 
   function addRoleCapability(uint8 role, bytes memory sig) private {
