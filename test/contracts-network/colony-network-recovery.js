9c72b7ed429e1b05f942ce1b59363a7253c4e44e
Alex Rea <git@area.io>
2023-06-26 21:27:46 +0100

    Some contract tidying, extra tests for coverage

--- test/contracts-network/colony-network-recovery.js
+++ test/contracts-network/colony-network-recovery.js
@@ -193,7 +193,6 @@ contract("Colony Network Recovery", (accounts) => {
       await checkErrorRevert(colonyNetwork.claimMiningReward(ADDRESS_ZERO), "colony-in-recovery-mode");
       await checkErrorRevert(colonyNetwork.startTokenAuction(ADDRESS_ZERO), "colony-in-recovery-mode");
       await checkErrorRevert(colonyNetwork.bridgeSkill(1), "colony-in-recovery-mode");
-      await checkErrorRevert(colonyNetwork.bridgeReputationUpdateLog(ADDRESS_ZERO, 0, 0), "colony-in-recovery-mode");
       await checkErrorRevert(colonyNetwork.bridgePendingReputationUpdate(ADDRESS_ZERO, 0), "colony-in-recovery-mode");
       await checkErrorRevert(colonyNetwork.addReputationUpdateLogFromBridge(ADDRESS_ZERO, ADDRESS_ZERO, 0, 0, 0), "colony-in-recovery-mode");
       await checkErrorRevert(colonyNetwork.addPendingReputationUpdate(0, ADDRESS_ZERO), "colony-in-recovery-mode");
