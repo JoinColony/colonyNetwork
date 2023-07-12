5943914f1d3acf18bc38349be7671d04481a2e9a
Alex Rea <git@area.io>
2023-07-12 11:29:06 +0100

    First pass at per-role approvals

--- test-smoke/colony-storage-consistent.js
+++ test-smoke/colony-storage-consistent.js
@@ -149,8 +149,8 @@ contract("Contract Storage", (accounts) => {
       console.log("miningCycleStateHash:", miningCycleStateHash);
       console.log("tokenLockingStateHash:", tokenLockingStateHash);
 
-      expect(colonyNetworkStateHash).to.equal("0xf5af8b7e645fe187c65f30082cd3af87183bca6d40d6f6aa15c1ba1a57c45b20");
-      expect(colonyStateHash).to.equal("0xb8787ae86c82f3e8ff6ddbb675b9c813b59c18cbcf4cc4c73a716311cee1030e");
+      expect(colonyNetworkStateHash).to.equal("0x3f09cbe7e74f60aeb2ae46e2053f88d72d189918cf13699b921b5e7200554616");
+      expect(colonyStateHash).to.equal("0x2422c452c5ed0d5d1a630a19529042e1e7b4479fc0988083b3423369ee8cc144");
       expect(metaColonyStateHash).to.equal("0x60b0189dea488416bdeed0de728d7659775d697219137827d84c83e97210e7e9");
       expect(miningCycleStateHash).to.equal("0xc9c36a48e266c74abc34de1ef3e522c9925d22941c41a307819c4460635bd260");
       expect(tokenLockingStateHash).to.equal("0x3dc9cdaf6f43272b4d4485689bcd4f42e3e5d46de02ce2b3b5b33bde51cde7ee");
