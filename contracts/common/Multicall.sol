65e11decd38e1b98c075f104194bbf6716557a69
Alex Rea <git@area.io>
2023-09-15 14:37:20 +0100

    Configure solhint for solidity >= 0.7.0, other easy fixes

--- contracts/common/Multicall.sol
+++ contracts/common/Multicall.sol
@@ -6,7 +6,7 @@ pragma experimental "ABIEncoderV2";
 import {MetaTransactionMsgSender} from "./MetaTransactionMsgSender.sol";
 
 abstract contract Multicall is MetaTransactionMsgSender {
-  bytes4 constant multicallSig = bytes4(keccak256("multicall(bytes[])"));
+  bytes4 constant MULTICALL_SIG = bytes4(keccak256("multicall(bytes[])"));
 
   function multicall(bytes[] calldata data) public returns (bytes[] memory results) {
     // First off, is this a metatransaction?
@@ -19,7 +19,7 @@ abstract contract Multicall is MetaTransactionMsgSender {
 
     results = new bytes[](data.length);
     for (uint256 i; i < data.length; i++) {
-      require(bytes4(data[i]) != multicallSig, "colony-multicall-cannot-multicall");
+      require(bytes4(data[i]) != MULTICALL_SIG, "colony-multicall-cannot-multicall");
       // Slither is technically right here, but only one is (fully) under the user's control, and I
       // don't think this type of pattern is exploitable here, anyway (because we're not hashing the
       // result and using it to verify something).
@@ -28,7 +28,7 @@ abstract contract Multicall is MetaTransactionMsgSender {
 
       if (!success) {
         // Next 5 lines from https://ethereum.stackexchange.com/a/83577
-        if (result.length < 68) revert();
+        if (result.length < 68) revert("multicall-silent-revert");
         assembly {
           result := add(result, 0x04)
         }
