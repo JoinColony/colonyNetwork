c22ff1146783c7fbd0d6291502443207f9fe8ff4
Daniel Kronovet <kronovet@gmail.com>
2024-02-15 14:54:15 -0800

    Temp

--- .solcover.crosschain.js
+++ .solcover.crosschain.js
@@ -1,13 +1,5 @@
 const config = require("./.solcover.js")
-const log = console.log;
-const { execSync } = require("child_process");
-
-const existingCompileComplete = config.onCompileComplete;
 
 config.istanbulFolder = `./coverage-cross-chain-${process.env.TRUFFLE_HOME ? "home" : "foreign"}`
-
-config.onCompileComplete = function() {
-	existingCompileComplete();
-}
 
 module.exports = config
