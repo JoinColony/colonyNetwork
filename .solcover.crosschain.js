fb7a730d7d6bf1a16bc271ef0a627c3440069472
Alex Rea <git@area.io>
2024-03-04 16:36:56 +0000

    Fix docker version on CircleCI for all jobs

--- .solcover.crosschain.js
+++ .solcover.crosschain.js
@@ -1,20 +1,5 @@
 const config = require("./.solcover.js")
 
-let chainId;
-// We configure the truffle coverage chain to have the same chainid as one of the
-// nodes we've started up, but on a different port
-// TODO: Actually query nodes, don't hard-code here, or work out how to get environment
-// variables in package.json to work here as I want.
-if (JSON.parse(process.env.TRUFFLE_FOREIGN)){
-  chainId = FORKED_XDAI_CHAINID + 1;
-} else {
-  chainId = FORKED_XDAI_CHAINID;
-}
-
-config.providerOptions.network_id = chainId;
-config.providerOptions._chainId = chainId;
-config.providerOptions._chainIdRpc = chainId;
-
 config.istanbulFolder = `./coverage-cross-chain-${process.env.TRUFFLE_HOME ? "home" : "foreign"}`
 
 module.exports = config
