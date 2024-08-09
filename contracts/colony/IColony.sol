316c93a23a45ab7572967000e58964e98a741cdc
Alex Rea <git@area.io>
2024-08-09 08:18:38 +0100

    Arbitrary transactions through proxy colonies

--- contracts/colony/IColony.sol
+++ contracts/colony/IColony.sol
@@ -61,6 +61,8 @@ interface IColony is IDSAuth, ColonyDataTypes, IRecovery, IBasicMetaTransaction,
     bytes memory _action
   ) external returns (bool success);
 
+  function makeProxyArbitraryTransaction(uint256 chainId,address _to,bytes memory action) external;
+
   /// @notice Execute arbitrary transactions on behalf of the Colony in series
   /// @param _targets Array of addressed to be targeted
   /// @param _actions Array of Bytes arrays encoding the function calls and arguments
