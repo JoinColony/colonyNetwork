0217857314408b824167e7a652efdd5766952035
Alex Rea <git@area.io>
2024-08-09 07:39:52 +0100

    Allow proxy colonies to pay funds

--- contracts/colony/ColonyAuthority.sol
+++ contracts/colony/ColonyAuthority.sol
@@ -134,6 +134,11 @@ contract ColonyAuthority is CommonAuthority {
     addRoleCapability(ARBITRATION_ROLE, "finalizeExpenditureViaArbitration(uint256,uint256,uint256)");
     addRoleCapability(ROOT_ROLE, "setColonyBridgeAddress(address)");
     addRoleCapability(ROOT_ROLE, "initialiseReputationMining(uint256,bytes32,uint256)");
+
+    // Colony ????
+    addRoleCapability(ARBITRATION_ROLE, "setExpenditurePayout(uint256,uint256,uint256,uint256,uint256,address,uint256)");
+    addRoleCapability(FUNDING_ROLE, "moveFundsBetweenPots(uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,address)");
+
   }
 
   function addRoleCapability(uint8 role, bytes memory sig) private {
