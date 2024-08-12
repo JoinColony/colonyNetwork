4795bbf0ae81437f022d039c2b2dd85cb21eb726
Alex Rea <git@area.io>
2024-08-12 11:42:10 +0100

    setup-bridging-contracts can handle multiple remote chains

--- packages/wormhole-relayer/index.ts
+++ packages/wormhole-relayer/index.ts
@@ -114,6 +114,7 @@ const loader = new TruffleLoader({
 
     let destinationBridge;
 
+<<<<<<< HEAD
     if (vaa.emitterChain === CHAIN_ID_ARBITRUM_SEPOLIA) {
       destinationBridge = colonyBridges[CHAIN_ID_SEPOLIA];
     } else if (vaa.emitterChain === CHAIN_ID_SEPOLIA) {
@@ -121,6 +122,83 @@ const loader = new TruffleLoader({
     } else {
       console.log("Unknown chain", vaa.emitterChain);
       return next();
+||||||| parent of 45f052f5 (setup-bridging-contracts can handle multiple remote chains)
+    app.spy("localhost:7073");
+    // app.useStorage(store);
+    // app.logger(console);
+
+    // Set up middleware
+    // app.use(logging(console)); // <-- logging middleware
+    app.use(providers(config));
+    // app.use(stagingArea());
+    // app.use(sourceTx());
+
+
+    // const colonyBridgeAddresses = {
+    //   [CHAIN_ID_ARBITRUM_SEPOLIA]: "0x633899227A3BC1f79de097149E1E3C8097c07b1a",
+    //   [CHAIN_ID_SEPOLIA]: "0x161944B5601a7d3004E20d4Ca823F710838Ea1be",
+    // };
+
+    const { TruffleLoader } = require("../package-utils");
+    const path = require("path");
+    const loader = new TruffleLoader({
+      contractRoot: path.resolve(__dirname, "..", "..", "artifacts", "contracts")
+    });
+
+    const colonyBridges = {};
+    const colonyBridgeContractDef = await loader.load({ contractDir: "bridging", contractName: "WormholeBridgeForColony" });
+    const ethers = require('ethers');
+    const privateKey = "0xfe6066af949ec3c2c88ac10f47907c6d4e200c37b28b5af49e7d0ffd5c301c5c";
+    for (const chainId of Object.keys(config.chains)) {
+      const colonyBridgeAddress = config.chains[chainId].colonyBridgeAddress;
+      const providerAddress = config.chains[chainId].endpoints[0];
+      const wallet = new ethers.Wallet(privateKey, new RetryProvider(providerAddress));
+      const nonceManager = new NonceManager(wallet);
+
+      colonyBridges[chainId] = new ethers.Contract(colonyBridgeAddress, colonyBridgeContractDef.abi, nonceManager);
+=======
+    app.spy("localhost:7073");
+    // app.useStorage(store);
+    // app.logger(console);
+
+    // Set up middleware
+    // app.use(logging(console)); // <-- logging middleware
+    app.use(providers(config));
+    // app.use(stagingArea());
+    // app.use(sourceTx());
+
+
+    // const colonyBridgeAddresses = {
+    //   [CHAIN_ID_ARBITRUM_SEPOLIA]: "0x633899227A3BC1f79de097149E1E3C8097c07b1a",
+    //   [CHAIN_ID_SEPOLIA]: "0x161944B5601a7d3004E20d4Ca823F710838Ea1be",
+    // };
+
+    const { TruffleLoader } = require("../package-utils");
+    const path = require("path");
+    const loader = new TruffleLoader({
+      contractRoot: path.resolve(__dirname, "..", "..", "artifacts", "contracts")
+    });
+
+    const colonyBridges = {};
+    const colonyBridgeContractDef = await loader.load({ contractDir: "bridging", contractName: "WormholeBridgeForColony" });
+    const ethers = require('ethers');
+    const privateKey = "0xfe6066af949ec3c2c88ac10f47907c6d4e200c37b28b5af49e7d0ffd5c301c5c";
+    for (const chainId of Object.keys(config.chains)) {
+      const colonyBridgeAddress = config.chains[chainId].colonyBridgeAddress;
+      const providerAddress = config.chains[chainId].endpoints[0];
+      const wallet = new ethers.Wallet(privateKey, new RetryProvider(providerAddress));
+      const nonceManager = new NonceManager(wallet);
+
+      colonyBridges[chainId] = new ethers.Contract(colonyBridgeAddress, colonyBridgeContractDef.abi, nonceManager);
+      const networkDetails = await wallet.provider.getNetwork();
+      console.log('networkDetails', networkDetails)
+      if (networkDetails.chainId !== config.chains[chainId].evmChainId) {
+        console.log('Network details do not match config for chain', chainId);
+        console.log('Got an evmChainId of', networkDetails.chainId, 'but expected', config.chains[chainId].evmChainId);
+        console.log('Exiting');
+        process.exit(1);
+      }
+>>>>>>> 45f052f5 (setup-bridging-contracts can handle multiple remote chains)
     }
 
     try {
@@ -147,6 +225,149 @@ const loader = new TruffleLoader({
 
   // add and configure any other middleware ..
 
+<<<<<<< HEAD
   // start app, blocks until unrecoverable error or process is stopped
   await app.listen();
 })();
+||||||| parent of 45f052f5 (setup-bridging-contracts can handle multiple remote chains)
+    // add a filter with a callback that will be
+    // invoked on finding a VAA that matches the filter
+
+    const colonyBridgeAddresses: {
+      [chainid: string]: string
+    } = {};
+
+    Object.keys(config.chains).forEach((chainid) => colonyBridgeAddresses[chainid] = config.chains[chainid].colonyBridgeAddress);
+    app.multiple( colonyBridgeAddresses,
+    async (ctx, next) => {
+        // console.log('callback');
+        const vaa = ctx.vaa;
+        if (!vaa) {
+          return next();
+        }
+        const hash = ctx.sourceTxHash;
+        // console.log(vaa);
+        console.log(
+          `Got a VAA with sequence: ${vaa.sequence} from with txhash: ${hash}`,
+        );
+
+        let destinationBridge;
+
+        if (vaa.emitterChain === CHAIN_ID_ARBITRUM_SEPOLIA) {
+          destinationBridge = colonyBridges[CHAIN_ID_SEPOLIA];
+        } else if (vaa.emitterChain === CHAIN_ID_SEPOLIA) {
+          destinationBridge = colonyBridges[CHAIN_ID_ARBITRUM_SEPOLIA];
+        } else {
+          console.log('Unknown chain', vaa.emitterChain);
+          return next();
+        }
+
+        try {
+          // TODO: Explicit gas limit is a nod to tests...
+          const tx = await destinationBridge.receiveMessage(ctx.vaaBytes, { gasLimit: 1000000 });
+          const r = await tx.wait();
+          console.log('bridged with txhash' + tx.hash)
+        } catch(err) {
+          console.log("ERROR", err);
+          console.log('trying estimate gas with', err.transaction.to, err.transaction.data);
+          try {
+
+            const errEst = await destinationBridge.provider.estimateGas({
+              to: err.transaction.to,
+              data: err.transaction.data
+            });
+            console.log('errEst', errEst.toString());
+          } catch(err2){
+            console.log('ERROR2', err2);
+          }
+        }
+
+        next();
+      },
+    );
+
+    // add and configure any other middleware ..
+
+    // start app, blocks until unrecoverable error or process is stopped
+    await app.listen();
+
+  })();
+
+=======
+    // add a filter with a callback that will be
+    // invoked on finding a VAA that matches the filter
+
+    const colonyBridgeAddresses: {
+      [chainid: string]: string
+    } = {};
+
+    Object.keys(config.chains).forEach((chainid) => colonyBridgeAddresses[chainid] = config.chains[chainid].colonyBridgeAddress);
+    app.multiple( colonyBridgeAddresses,
+    async (ctx, next) => {
+        // console.log('callback');
+        const vaa = ctx.vaa;
+        if (!vaa) {
+          return next();
+        }
+        const hash = ctx.sourceTxHash;
+        const [destinationEvmChainId, destinationAddress, payload] = (new ethers.utils.AbiCoder).decode(['uint256', 'address', 'bytes'],`0x${vaa.payload.toString('hex')}`);
+        console.log('destinationEvmChainId', destinationEvmChainId);
+        console.log('destinationAddress', destinationAddress);
+        console.log('payload', payload);
+
+
+        console.log(
+          `Got a VAA with sequence: ${vaa.sequence} from with txhash: ${hash}`,
+        );
+
+        const destinationChainConfig = Object.values(config.chains).find((c) => c.evmChainId === destinationEvmChainId.toNumber())
+        if (!destinationChainConfig) {
+          console.log('No destination chain config found for chain id', destinationEvmChainId.toNumber());
+          return next();
+        }
+
+        if (!destinationChainConfig.payForGas) {
+          console.log('We do not pay for gas on destination chain. Skipping');
+          return next();
+        }
+
+        const destinationWormholeId = Object.keys(config.chains).find((wormholeChainId) => { return config.chains[wormholeChainId].evmChainId === destinationEvmChainId.toNumber() });
+        if (!destinationWormholeId) {
+          console.log('No wormhole chain id found for destination chain id', destinationEvmChainId.toNumber());
+          return next();
+        }
+
+        const destinationBridge = colonyBridges[destinationWormholeId];
+
+        try {
+          // TODO: Explicit gas limit is a nod to tests...
+          const tx = await destinationBridge.receiveMessage(ctx.vaaBytes, { gasLimit: 1000000 });
+          const r = await tx.wait();
+          console.log('bridged with txhash' + tx.hash)
+        } catch(err) {
+          console.log("ERROR", err);
+          console.log('trying estimate gas with', err.transaction.to, err.transaction.data);
+          try {
+
+            const errEst = await destinationBridge.provider.estimateGas({
+              to: err.transaction.to,
+              data: err.transaction.data
+            });
+            console.log('errEst', errEst.toString());
+          } catch(err2){
+            console.log('ERROR2', err2);
+          }
+        }
+
+        next();
+      },
+    );
+
+    // add and configure any other middleware ..
+
+    // start app, blocks until unrecoverable error or process is stopped
+    await app.listen();
+
+  })();
+
+>>>>>>> 45f052f5 (setup-bridging-contracts can handle multiple remote chains)
