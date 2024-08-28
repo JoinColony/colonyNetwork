f49a90924b7cb418b588172346126cc85ec3d21b
Alex Rea <git@area.io>
2024-08-28 12:32:48 +0100

    Update storage-layouts for proxy contracts

--- contracts/colonyNetwork/ColonyNetworkDeployer.sol
+++ contracts/colonyNetwork/ColonyNetworkDeployer.sol
@@ -333,12 +333,4 @@ contract ColonyNetworkDeployer is ColonyNetworkStorage {
     );
     return address(etherRouter);
   }
-
-  function isContract(address addr) internal view returns (bool) {
-    uint256 size;
-    assembly {
-      size := extcodesize(addr)
-    }
-    return size > 0;
-  }
 }
