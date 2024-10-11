195fc7582b7b5abf556ef0d51281c4fb8ab80ebb
Alex Rea <git@area.io>
2024-10-11 17:55:13 +0100

    Root has to allow domains to receive reputation-earning tokens

--- contracts/colony/ColonyFunding.sol
+++ contracts/colony/ColonyFunding.sol
@@ -130,13 +130,59 @@ contract ColonyFunding is
     nonRewardPotsTotal[_token] += remainder;
 
     fundingPots[0].balance[_token] += feeToPay;
-    fundingPots[fundingPotId].balance[_token] += remainder;
+
+    if (tokenEarnsReputationOnPayout(_token)) {
+      // If token earns reputation, we only allow up to the approved amount to be received
+      uint256 approvedAmount = domainReputationTokenApprovals[_domainId][_token];
+      if (approvedAmount < remainder) {
+        fundingPots[fundingPotId].balance[_token] += approvedAmount;
+        Domain storage rootDomain = domains[1];
+        // And the rest goes to the root pot
+        fundingPots[rootDomain.fundingPotId].balance[_token] += remainder - approvedAmount;
+        domainReputationTokenApprovals[_domainId][_token] = 0;
+        emit DomainFundsClaimed(msgSender(), _token, _domainId, feeToPay, approvedAmount);
+        emit ColonyFundsClaimed(msgSender(), _token, 0, remainder - approvedAmount);
+      } else {
+        fundingPots[fundingPotId].balance[_token] += remainder;
+        domainReputationTokenApprovals[_domainId][_token] -= remainder;
+        emit DomainFundsClaimed(msgSender(), _token, _domainId, feeToPay, remainder);
+      }
+    } else {
+      fundingPots[fundingPotId].balance[_token] += remainder;
+      emit DomainFundsClaimed(msgSender(), _token, _domainId, feeToPay, remainder);
+    }
 
     // Claim funds
 
     DomainTokenReceiver(domainTokenReceiverAddress).transferToColony(_token);
+  }
 
-    emit DomainFundsClaimed(msgSender(), _token, _domainId, feeToPay, remainder);
+  function tokenEarnsReputationOnPayout(address _token) internal view returns (bool) {
+    return _token == token;
+  }
+
+  function editAllowedDomainTokenReceipt(
+    uint256 _domainId,
+    address _token,
+    uint256 _amount,
+    bool _add
+  ) public stoppable auth {
+    require(domainExists(_domainId), "colony-funding-domain-does-not-exist");
+    require(tokenEarnsReputationOnPayout(_token), "colony-funding-token-does-not-earn-reputation");
+    if (_add) {
+      domainReputationTokenApprovals[_domainId][_token] += _amount;
+    } else {
+      domainReputationTokenApprovals[_domainId][_token] -= _amount;
+    }
+  }
+
+  function getAllowedDomainTokenReceipt(
+    uint256 _domainId,
+    address _token
+  ) public view returns (uint256) {
+    require(domainExists(_domainId), "colony-funding-domain-does-not-exist");
+    require(tokenEarnsReputationOnPayout(_token), "colony-funding-token-does-not-earn-reputation");
+    return domainReputationTokenApprovals[_domainId][_token];
   }
 
   function getNonRewardPotsTotal(address _token) public view returns (uint256) {
