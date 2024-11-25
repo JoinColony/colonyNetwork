cefc2d4214c737606a3a15cb13794db2c951a68a
Alex Rea <git@area.io>
2024-11-25 17:29:56 +0000

    More LiFi guards/tests

--- contracts/colony/ColonyFunding.sol
+++ contracts/colony/ColonyFunding.sol
@@ -232,6 +232,8 @@ contract ColonyFunding is
     // TODO: Colony Network fee
     Domain storage domain = domains[_domainId];
 
+    uint256 priorApproval = ERC20Extended(_token).allowance(address(this), LIFI_ADDRESS);
+
     // Check the domain has enough for what is
     if (_token == address(0x0)) {
       require(
@@ -253,10 +255,24 @@ contract ColonyFunding is
     decrementFundingPotBalance(domain.fundingPotId, block.chainid, _token, _amount);
     decrementFundingPotBalance(domain.fundingPotId, block.chainid, address(0x0), _value);
 
-    require(ERC20Extended(_token).approve(LIFI_ADDRESS, _amount), "colony-approve-failed");
+    require(
+      ERC20Extended(_token).approve(LIFI_ADDRESS, _amount + priorApproval),
+      "colony-approve-failed"
+    );
     (bool success, ) = LIFI_ADDRESS.call{ value: _value }(_txdata);
 
     require(success, "colony-exchange-tokens-failed");
+
+    // Check the allowances afterwards, ensuring that at least some of the tokens we approved were spent by
+    // the LiFi transaction - if not, a different token might have been spent, so revert.
+    uint256 postApproval = ERC20Extended(_token).allowance(address(this), LIFI_ADDRESS);
+    require(_amount + priorApproval > postApproval, "colony-unexpected-exchange");
+    require(postApproval >= priorApproval, "colony-more-than-intended-allowance-used");
+
+    // If the LiFi transaction didn't use all the tokens, reduce the allowance back to what it was before
+    if (postApproval > priorApproval) {
+      require(ERC20Extended(_token).approve(LIFI_ADDRESS, priorApproval), "colony-post-exchange-approve-failed");
+    }
   }
 
   function exchangeProxyHeldTokensViaLiFi(
